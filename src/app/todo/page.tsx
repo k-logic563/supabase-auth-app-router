6929396ea9cefd8ddaa8470f89d6a0115560bfde
k-logic24 <k.log.ic563@gmail.com>
2023-05-08 22:48:30 +0900

    wip

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,11 +1,33 @@
-import { TodoList } from '@src/components/todo/List'
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { headers, cookies } from "next/headers";
 
-const Todo = () => {
+import type { Database } from "@src/types/database";
+
+const fetchTodos = async () => {
+  const supabase = createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  })
+  const { data, error } = await supabase
+    .from('todos')
+    .select()
+    .order('created_at', { ascending: true })
+  if (error) {
+    throw new Error('Network response was not ok')
+  }
+  return data
+}
+
+const Todo = async () => {
+  const todos = await fetchTodos()
   return (
-    <>
-      {/*@ts-ignore*/}
-      <TodoList />
-    </>
+    <ul>
+      { todos.map(todo => (
+        <li key={todo.id}>
+          {todo.title}
+        </li>
+      ))}
+    </ul>
   )
 }
 
